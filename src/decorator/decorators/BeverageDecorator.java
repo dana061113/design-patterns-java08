617f2396c3c99a9401603dcd349a9f74a708162a
dana061113 <danakobylandykyzy@gmail.com>
2026-03-22 19:39:33 +0500

    Create BeverageDecorator.java

---
+++ src/decorator/decorators/BeverageDecorator.java
@@ -0,0 +1,11 @@
+package decorator.decorators;
+
+import decorator.beverages.Beverage;
+
+public abstract class BeverageDecorator implements Beverage {
+    protected Beverage beverage;
+
+    public BeverageDecorator(Beverage beverage) {
+        this.beverage = beverage;
+    }
+}
