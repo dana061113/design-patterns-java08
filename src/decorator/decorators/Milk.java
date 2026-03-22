c5a719a83b5083d8f872b7d30e67609496423af9
dana061113 <danakobylandykyzy@gmail.com>
2026-03-22 19:40:05 +0500

    Create Milk.java

---
+++ src/decorator/decorators/Milk.java
@@ -0,0 +1,18 @@
+package decorator.decorators;
+
+import decorator.beverages.Beverage;
+
+public class Milk extends BeverageDecorator {
+
+    public Milk(Beverage beverage) {
+        super(beverage);
+    }
+
+    public String getDescription() {
+        return beverage.getDescription() + ", Milk";
+    }
+
+    public double cost() {
+        return beverage.cost() + 0.5;
+    }
+}
