62dc153e08f7e77688aa816ff644ebd6f603a889
dana061113 <danakobylandykyzy@gmail.com>
2026-03-22 19:52:56 +0500

    Create CryptoPaymentService.java

---
+++ src/adapter/crypto/CryptoPaymentService.java
@@ -0,0 +1,9 @@
+package adapter.crypto;
+
+public class CryptoPaymentService {
+
+    // Сыртқы крипто-сервис әдісі
+    public void sendCrypto(double value) {
+        System.out.println("Crypto payment: $" + value);
+    }
+}
