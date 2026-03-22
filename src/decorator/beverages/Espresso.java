6e44307c80ca561d040a2464b23297c28b4caa32
dana061113 <danakobylandykyzy@gmail.com>
2026-03-22 19:35:51 +0500

    Create Espresso.java

---
+++ src/decorator/beverages/Espresso.java
@@ -0,0 +1,11 @@
+package decorator.beverages;
+
+public class Espresso implements Beverage {
+    public String getDescription() {
+        return "Espresso";
+    }
+
+    public double cost() {
+        return 2.0;
+    }
+}
